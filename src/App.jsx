87cc41b7c70f9abc6bdf3459513dd9234b8feeec
ahmedgamil24 <agemail5050@gmail.com>
2026-02-23 23:01:40 +0200

    Initial commit - ecommerce frontend setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Camera } from "lucide-react"
+
+
+function App() {
+
+  return (
+    <>
+    <div className="flex justify-around items-center">
+      <h1 className="bg-amber-200 px-1.5 py-1.5">Ahmed Gamil</h1>
+      <Camera color="red" size={48} />
+    </div>
+    </>
+  )
+}
+
+export default App
