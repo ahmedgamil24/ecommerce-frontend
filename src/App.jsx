cf9101952852535975e7ae936e0c35c9b354f919
ahmedgamil24 <agemail5050@gmail.com>
2026-02-25 22:06:09 +0200

    update pacage.json file

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
         <h1 className="bg-amber-200 px-1.5 py-1.5">Ahmed Gamil</h1>
         <Camera color="red" size={48} />
       </div>
-      <div className="flex w-52 flex-col gap-4 ml-auto mr-auto mt-3">
+      <div className="flex w-52 flex-col gap-4 m-auto mt-3">
         <div className="skeleton h-32 w-full"></div>
         <div className="skeleton h-4 w-28"></div>
         <div className="skeleton h-4 w-full"></div>
