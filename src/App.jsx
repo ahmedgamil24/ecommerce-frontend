da0a3ab90d5a67587cbaeaf3ae2b644fa27899d9
ahmedgamil24 <agemail5050@gmail.com>
2026-03-06 12:14:47 +0200

    updating navBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import { Camera } from "lucide-react";
+import { Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <div className="flex justify-around items-center">
-        <h1 className="bg-amber-200 px-1.5 py-1.5">Ahmed Gamil</h1>
-        <Camera color="red" size={48} />
-      </div>
-      <div className="flex w-52 flex-col gap-4 m-auto mt-3">
-        <div className="skeleton h-32 w-full"></div>
-        <div className="skeleton h-4 w-28"></div>
-        <div className="skeleton h-4 w-full"></div>
-        <div className="skeleton h-4 w-full"></div>
-      </div>
+      <Header />
+      <Routes>
+        {/* <Route path="/" element={<Home />}/> */}
+      </Routes>
+
     </>
   );
 }
