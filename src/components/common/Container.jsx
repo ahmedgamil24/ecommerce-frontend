cf992f9df7876394ef2f52c1ba0e1b3e125ae13d
ahmedgamil24 <agemail5050@gmail.com>
2026-03-07 22:34:16 +0200

    updating measurment of container component

--- src/components/common/Container.jsx
+++ src/components/common/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Container = ({children, className=""}) => {
   return (
-    <div className={`w-full  mx-auto px-5 max-w-[1400px] sm:px-6 lg:px-[75px] ${className}`}>
+    <div className={`w-full  mx-auto px-5 max-w-350 sm:px-6 lg:px-18.75 ${className}`}>
         {children}
     </div>
   )
