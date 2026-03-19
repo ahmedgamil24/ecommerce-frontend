5bf04887217aa0471cfcce02688809382b8ef0e3
ahmedgamil24 <agemail5050@gmail.com>
2026-03-20 01:31:41 +0200

    updating banner ads component

--- src/components/BannerAds.jsx
+++ src/components/BannerAds.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
 const BannerAds = () => {
+    const images = [
+        {src: "https://api.spicezgold.com/download/file_1734525879105_banner-7.jpg"},
+        {src: "https://api.spicezgold.com/download/file_1734525868575_banner-9.jpg"},
+        {src: "https://api.spicezgold.com/download/file_1734525855497_banner-5.jpg"},
+    ]
   return (
-    <div>BannerAds</div>
+    <div>
+        {images.map((img) => (
+            <div key={img.src}>
+                <img src={img.src} alt="banner image" />
+            </div>
+        ))}
+    </div>
   )
 }
 
